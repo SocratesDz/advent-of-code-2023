e3b3f477cf5a5788323752283c3c77d472d86a8a
Sócrates Díaz <sdsxd1800@gmail.com>
2024-01-27 15:52:05 -0400

    feat: completed puzzle 4 part 2

--- src/puzzle4.rs
+++ src/puzzle4.rs
@@ -82,6 +82,37 @@ impl TryFrom<&str> for Scratchcard {
     }
 }
 
+pub fn answer_part_1(scratchcards: &[Result<Scratchcard, ScratchcardError>]) -> i32 {
+    // Sum all scores of every scratchcard
+    scratchcards
+        .iter()
+        .map(|card| card.as_ref().map(|c| c.get_score()))
+        .fold(0, |acc, score| acc + score.unwrap_or(0)) as i32
+}
+
+pub fn answer_part_2(scratchcards: &[Result<Scratchcard, ScratchcardError>]) -> i32 {
+    let mut card_indexes: Vec<usize> = scratchcards
+        .iter()
+        .enumerate()
+        .map(|(idx, _)| idx)
+        .collect();
+
+    let mut card_index = 0;
+
+    while card_index < card_indexes.len() {
+        let index = card_indexes[card_index];
+        let card = scratchcards[index].as_ref().expect("Wrong scratchcard");
+        let matching_count = card.get_matching_count();
+
+        for i in (index + 1)..=(index + matching_count as usize) {
+            card_indexes.push(i);
+        }
+
+        card_index += 1
+    }
+    card_indexes.len() as i32
+}
+
 pub fn answer() -> (i32, i32) {
     let input = fs::read_to_string("puzzle4.txt").expect("Puzzle file not found.");
 
@@ -90,25 +121,13 @@ pub fn answer() -> (i32, i32) {
         .map(Scratchcard::try_from)
         .collect::<Vec<Result<Scratchcard, ScratchcardError>>>();
 
-    // Sum all scores of every scratchcard
-    let answer_part_1 = &scratchcards
-        .iter()
-        .map(|card| card.as_ref().map(|c| c.get_score()))
-        .fold(0, |acc, score| acc + score.unwrap_or(0));
-
-    // Sum all scores (plus one) of every winning scratchcard
-    let answer_part_2 = &scratchcards
-        .iter()
-        .map(|card| card.as_ref().map(|c| c.get_score() + 1))
-        .fold(0, |acc, card_count| acc + card_count.unwrap_or(0));
-
-    (*answer_part_1 as i32, *answer_part_2 as i32)
+    (answer_part_1(&scratchcards), answer_part_2(&scratchcards))
 }
 
 #[cfg(test)]
 mod tests {
 
-    use crate::puzzle4::{Scratchcard, ScratchcardError};
+    use crate::puzzle4::{answer_part_1, answer_part_2, Scratchcard, ScratchcardError};
 
     #[test]
     fn test_parse_scorecard() {
@@ -128,11 +147,12 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#;
 
-        let answer = puzzle_input
+        let original_scratchcards = puzzle_input
             .lines()
             .map(Scratchcard::try_from)
-            .map(|card| card.map(|c| c.get_score()))
-            .fold(0, |acc, count| acc + count.unwrap_or(0));
+            .collect::<Vec<Result<Scratchcard, ScratchcardError>>>();
+
+        let answer = answer_part_1(&original_scratchcards);
 
         assert!(answer == 13)
     }
@@ -152,28 +172,8 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#;
             .map(Scratchcard::try_from)
             .collect::<Vec<Result<Scratchcard, ScratchcardError>>>();
 
-        let mut card_indexes: Vec<usize> = original_scratchcards
-            .iter()
-            .enumerate()
-            .map(|(idx, _)| idx)
-            .collect();
+        let answer = answer_part_2(&original_scratchcards);
 
-        let mut card_index = 0;
-
-        while card_index < card_indexes.len() {
-            let index = card_indexes[card_index];
-            let card = original_scratchcards[index]
-                .as_ref()
-                .expect("Wrong scratchcard");
-            let matching_count = card.get_matching_count();
-
-            for i in (index + 1)..=(index + matching_count as usize) {
-                card_indexes.push(i);
-            }
-
-            card_index += 1
-        }
-
-        assert!(card_indexes.len() == 30);
+        assert!(answer == 30);
     }
 }
